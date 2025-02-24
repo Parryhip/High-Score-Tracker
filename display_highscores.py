6a160b90f0a6892846a34b2795db0b7684c44dd1
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-02-24 10:51:01 -0700

    wow

---
+++ display_highscores.py
@@ -0,0 +1,7 @@
+#--------------------------------------------------------------------Matthew's Code-------------------------------------------------------------------------------------------#
+def print_tictactoe_leaderboard():
+    count = 0
+    while count <= 10:
+        #take the index of that in tictactoe_leaders
+        count += 1
+        #make it so that when they want to leave
