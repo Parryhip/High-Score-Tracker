6a1c1661294f4a38e401306fbb74c6d20f313940
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-02-25 10:23:13 -0700

    finshed matching function

--- games.py
+++ games.py
@@ -1,5 +1,7 @@
 #Nicholas Larsen
+import pygame
 
+#Looks for a match in tic-tac-toe
 def match(board, letter):
     for row in board:
         matched = True
@@ -17,9 +19,34 @@ def match(board, letter):
         if matched:
             return True
     matched = True
+    #Uses ranges to select the diagonal squares on the board and checks if they match
     for square in range(3):
         if board[square][square] != letter:
             matched = False
-        
+    if matched:
+        return True
+    matched = True
+    for square in range(3):
+        if board[square][square - 2] != letter:
+            matched = False
+    if matched:
+        return True
+    #Returns false if no matches were found
+    return False
 
+def play_again():
+    while True:
+        answer = input("Would you like to play that game again? y/n:")
+        if answer.lower() == 'y':
+            return True
+        elif answer.lower() == 'n':
+            return False
+        else:
+            print('Please enter y or n.')
+
+#Plays the game, tic tac toe.
+def tictactoe():
+    while True:
+        pass
+    #CONTINUE HERE
         
