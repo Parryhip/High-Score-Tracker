7d5b5709ad1d7bb695d6363b8707658ceaf3a23e
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-02-26 10:50:29 -0700

    i can finish tonight

--- display_highscores.py
+++ display_highscores.py
@@ -1,41 +1,43 @@
 #-------------------------------------------------------------------------------------------Matthew's Code-------------------------------------------------------------------------------------------#
 
-
 def print_tictactoe_leaderboard():
+    from retrieve_high_scores import retrieve_tic_tac_toe as retrieve 
     count = 0
+    high_scores = retrieve()
     while count <= 10:
-        with open("tic_tac_toe_high_scores.txt", "r") as file:
-            #for line in file:
-                #print(line)
-                #count += 1
+        for user in high_scores:
+            print(count,".",user, ":",high_scores[user])
+            count += 1
     else:
 
-        
         #make it so that when they want to leave, they can just press enter
+
 
 def print_numguessing_leaderboard():
+    from retrieve_high_scores import retrieve_num_guessing as retrieve 
     count = 0
+    high_scores = retrieve()
     while count <= 10:
-        with open("num_guessing_high_scores.txt", "r") as file:
-            #for line in file:
-                #print(line)
-                #count += 1
+        for user in high_scores:
+            print(count,".",user, ":",high_scores[user])
+            count += 1
     else:
 
-        
         #make it so that when they want to leave, they can just press enter
+
 
 def print_reactionspeed_leaderboard():
+    from retrieve_high_scores import retrieve_reaction_speed as retrieve 
     count = 0
+    high_scores = retrieve()
     while count <= 10:
-        with open("reaction_speed_high_scores.txt", "r") as file:
-            #for line in file:
-                #print(line)
-                #count += 1
+        for user in high_scores:
+            print(count,".",user, ":",high_scores[user])
+            count += 1
     else:
 
-        
         #make it so that when they want to leave, they can just press enter
+
 
 def choose_leaderboard():
     while True:
